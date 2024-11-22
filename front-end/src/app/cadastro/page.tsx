bb8659e3bdc374b1d252294144fce5c6ca6dce9d
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-11-22 09:43:02 -0300

    add: user validation

--- front-end/src/app/cadastro/page.tsx
+++ front-end/src/app/cadastro/page.tsx
@@ -1,24 +1,61 @@
+'use client'
+
 import Image from "next/image";
 
 import { Kanit } from "next/font/google";
-import React from "react";
+import React, { useState } from "react";
 
+import "./style.css";
 
 import Button from "@/components/Button";
 import Input from "@/components/Input/Input";
 import Link from "next/link";
 import GoBack from "@/components/GoBack";
 
+import { validateUserToCreate } from "../../schemas/UserSchema";
+
 const kanit = Kanit({
 	weight: '400',
 	subsets: ['latin']
 });
 
+interface Error {
+	message: string;
+	path: string | number;
+}
 export default function Cadastro() {
+	const [ name, setName ] = useState("");
+	const [ email, setEmail ] = useState("");
+	const [ password, setPassword ] = useState("");
+	const [ confirmPassword, setConfirmPassword ] = useState("");
+
+	const messageError: Array<Error> = [];
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		if(password !== confirmPassword) {
+			messageError.push({
+				message: "As senhas digitadas devem ser iguais",
+				path: "confirmPassword",
+			})
+		
+		} else {
+			const user = validateUserToCreate({name, email, password, confirmPassword});
+	
+			if (!user.success) {
+				user.error?.issues.map((issue) => {
+					messageError.push({
+						message: issue.message,
+						path: issue.path[0],
+					});
+				});
+			}
+		}
+	};
 	return (
 		<>
 			<GoBack />
-			<div className="w-screen flex flex-col items-center justify-center">
+			<div className="registerForm">
 			
 				<Image 
 					src={'/img/logov4.svg'}
@@ -29,48 +66,55 @@ export default function Cadastro() {
 					className="ml-24"
 				/>				
 
-				<form className="h-3/4 w-5/12 rounded-3xl mt-0 flex flex-col bg-white p-7 gap-6 shadow-2xl">
-					<h1 className={kanit.className} style={style.title}>Cadastro</h1>
+				<form onSubmit={handleSubmit}
+				className="h-fit w-5/12 rounded-3xl mt-0 flex flex-col bg-white p-7 shadow-2xl mb-3">
+					<h1 className={kanit.className}>Cadastro</h1>
 					
 					<div>
 						<Input
-							label="Nome"
+							label="Nome*"
 							placeholder="Ex: José Santos"
 							type="text"
+							onChange={(e) => setName(e.target.value)}
 						/>
 
 						<Input
-							label="Email"
+							label="Email*"
 							placeholder="Ex: jose.santos@email.com"
 							type="email"
+							onChange={(e) => setEmail(e.target.value)}
 						/>
 
 						<Input
-							label="Senha"
+							label="Senha*"
 							placeholder="Digite sua senha"
 							type="password"
+							onChange={(e) => setPassword(e.target.value)}
 						/>
 
 						<Input
-							label="Confirme sua senha"
+							label="Confirme sua senha*"
 							placeholder="Digite novamente sua senha"
 							type="password"
+							onChange={(e) => setConfirmPassword(e.target.value)}
 						/>
 					</div>
-					
-					
 
 					<div className="flex flex-col items-center self-center gap-7">
 						<Link
 							href="/login"
 							className={kanit.className}
-							style={style.link}
+							style={{
+								color: "#1E90FF",
+								textDecoration: "underline"
+							}}
 
 						>Já tem uma conta? Clique aqui para acessar</Link>
 
 						<Button 
 							color="#006400"
 							text="Criar conta"
+							type="submit"
 						/>
 					</div>
 					
@@ -80,15 +124,3 @@ export default function Cadastro() {
 
 	);
 }
-
-const style = {
-	title: {
-		color: "#000000",
-		fontSize: "28px",
-	},
-
-	link: {
-		color: "#1E90FF",
-		textDecoration: "underline",
-	}
-};
--- front-end/src/components/GoBack.tsx
+++ front-end/src/components/GoBack.tsx
@@ -1,9 +1,8 @@
-import { Kanit } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function GoBack() {
     return(
-        <Link href={"/"}> <Image src={'/img/arrowBack.svg'} width={80} height={80} alt="Go-Back-Img" className="pl-3 pt-3"/></Link>
+        <Link href={"/"}> <Image src={'/img/arrowBack.svg'} width={65} height={65} alt="Go-Back-Img" className="pl-3 pt-3"/></Link>
     )
 }
