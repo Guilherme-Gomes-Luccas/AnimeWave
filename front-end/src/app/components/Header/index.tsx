5ee874b62439e4139a4e6e8373019231c114a148
Rafael Campos <rafael.campos.m@hotmail.com>
2024-11-27 21:14:29 -0300

    Criação do Header

--- front-end/src/app/components/Header/index.tsx
+++ front-end/src/app/components/Header/index.tsx
@@ -1,26 +1,36 @@
 import React from "react";
 import Image from "next/image";
 import logo from "./logo.svg";
+import "./style.css"
+import Link from "next/link";
 
 
 
 
 export default function Header() {
     return (
-        <div className='bg-header-blue w-full bg-[#--dark-blue, linear-gradient(90deg, rgba(5, 28, 78, 0.85) 11%, rgba(5, 28, 80, 0.95) 53%, rgba(1, 19, 57, 0.85) 86%)]'    >
-            <Image
-                src={logo}
-                width={480}
-                height={480}
-                alt="logo-animeWave"
-            />
+        <div className='flex bg-header-blue w-full bg-blue-950'>
+          
+                <Image
+                    src={logo}
+                    width={480}
+                    height={400}
+                    alt="logo-animeWave"
+                />
+
+    
+      
+            <div className="flex justify-end ">
+                <Link href={""} className="text-white"> Cadastro</Link>
+                <Link href={""} className="text-white"> Login</Link>
+            </div>
         </div>
     )
 }
 
 const style = {
     header: {
-        backgroundColor:'#black',
+        backgroundColor: '#black',
     }
-    
+
 };
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header";
 
+
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Teste</h1>
+    <div className="flex flex-col">
       <Header/>
+     
     </div>
-
   );
-  
 }
