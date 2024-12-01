d731aa0eb6ef6a966cd1c360126a963dc4b399df
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-11-30 22:52:35 -0300

    using cookies to JWT token

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const accessToken = (await cookies()).get('accessToken');
+  const refreshToken = (await cookies()).get('refreshToken');
+
+	console.log(accessToken, refreshToken);
   return (
     <div className="flex flex-col">
       <Link href="/login">Login</Link>
