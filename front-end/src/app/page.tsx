7d6c80f61c31264b30c448207f9f3c3b47cdbef9
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-12-02 17:45:00 -0300

    manipulando cookies

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -3,10 +3,35 @@ import Link from "next/link";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  const accessToken = (await cookies()).get('accessToken');
+  let accessToken = (await cookies()).get('accessToken');
   const refreshToken = (await cookies()).get('refreshToken');
 
-	console.log(accessToken, refreshToken);
+  console.log(accessToken);
+  const getSession = async () => {
+    
+    const session = await fetch('http://localhost:3001/session', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${accessToken?.value}`
+      }
+    });
+
+    if (!session.ok) {
+      const refresh = await fetch('http://localhost:3001/refresh', {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Authorization': `Bearer ${refreshToken?.value}`
+        },
+      });
+
+      accessToken = (await refresh.json()).accessToken;
+      console.log(accessToken);
+    }
+  }
+
+  await getSession();
+
   return (
     <div className="flex flex-col">
       <Link href="/login">Login</Link>
