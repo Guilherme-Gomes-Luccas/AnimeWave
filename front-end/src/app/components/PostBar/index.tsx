f1f0bcb66159fd8bb8a8d01debf2d25d45ac077d
RAFAEL <cg3022943@labA205m16.IFSP>
2024-12-04 22:13:23 -0300

    criação da tela 'post'

---
+++ front-end/src/app/components/PostBar/index.tsx
@@ -0,0 +1,6 @@
+
+export default function PostBar() {
+    return(
+        <div className="bg-black w-2/12 h-screen"></div>
+    )
+}
---
+++ front-end/src/app/components/SideBar/index.tsx
@@ -0,0 +1,8 @@
+
+export default function SideBar() {
+    return(
+        <div className="bg-black w-2/12 h-screen">
+        
+        </div>
+    )
+}
