c4985cf06fd510bf28361e1f182b347d6005d7f8
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-12-04 21:01:00 -0300

    editing header

--- front-end/src/app/components/Header/index.tsx
+++ front-end/src/app/components/Header/index.tsx
@@ -8,16 +8,16 @@ export default function Header() {
     return (
         <div className='flex justify-between bg-header-blue w-full h-24 bg-blue-950 '>
           
-                <Image
-                    className="flex justify-end self-baseline px-9 pt-9 "
-                    src={logo}
-                    width={277}
-                    height={72}
-                    alt="logo-animeWave"
-                />
+            <Image
+                className="mt-2"
+                src={logo}
+                width={300}
+                height={300}
+                alt="logo-animeWave"
+            />
             <div className="flex justify-center self-center  px-28 font-kanit text-xl ">
-                <Link href={""} className="text-white px-4 mx-14   "> Cadastro</Link>
-                <Link href={""} className="text-white px-4"> Login</Link>
+                {/* <Link href={""} className="text-white px-4 mx-14   "> Cadastro</Link> */}
+                {/* <Link href={""} className="text-white px-4"> Login</Link> */}
             </div>
         </div>
     )
