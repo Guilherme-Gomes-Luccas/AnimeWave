a1750d167378fd088c94656c62186bfcc844a47a
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-12-08 18:26:14 -0300

    update refresh token

--- back-end/src/controllers/auth/config/generateRefreshToken.ts
+++ back-end/src/controllers/auth/config/generateRefreshToken.ts
@@ -13,7 +13,7 @@ export const generateRefreshToken = (user: User) => {
     process.env.TOKEN_KEY,
     {
       subject: user.public_id,
-      expiresIn: '60s',
+      expiresIn: '1d',
     },
   );
 
